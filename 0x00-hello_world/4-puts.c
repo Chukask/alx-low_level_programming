4a55642f2f43bfab83e2a6c7d0b640700b789b17
Chukask <francisoodezuluigbo@igbo@gmail.com>
2023-09-09 16:58:14 -0700

    not no

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- * Hello .C
+ * main - entry point
+ * return:  always 0 (success)
  */
 int main(void)
-{   
-         puts ("\"Programming is like building a multilingual puzzle");
+{
+         puts("\"Programming is like building a multilingual puzzle");
 	 return (0);
 }
