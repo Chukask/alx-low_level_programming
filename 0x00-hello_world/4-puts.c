787f0d7015b55871dd3e296fd6da700bb5ef75ce
Chukask <francisoodezuluigbo@igbo@gmail.com>
2023-09-09 15:47:44 -0700

    not now

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,6 @@
 #include<stdio.h>
-/**
- * main - Entry point
- * 
- * Return: Always o (Success)
- * /
  int main(void)
- {   
+{   
          puts("\"Programming is like building a multilingual puzzle");
-	 return (o);
- }
+	 return 0;
+}
