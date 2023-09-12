5fe8c131e559ee4459ec3a869b998ab82c459897
Chukask <francisoodezuluigbo@igbo@gmail.com>
2023-09-12 09:31:16 -0700

    keep going

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,18 +4,17 @@
  * Return: 0
  */
 int main(void)
-        {
+{
 	int a;
 
 	for (a = 0; a < 10; a++)
 	{
 		putchar(a + '0');
 		if (a < 9)
-		{	
+		{
 			putchar(',');
 		}
 	}
 	putchar('\n');
 	return (0);
-	}
-
+}
