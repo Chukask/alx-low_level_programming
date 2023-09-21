51d9b28eb451aaad28b57646ac70f5eede64ac37
Chukask <francisoodezuluigbo@igbo@gmail.com>
2023-09-21 15:35:55 -0700

    krrp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,15 +8,13 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i;
+	int b = 0;
 
-	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0')
+	for (i = 0; s1[i] !=  '\0' && b == 0; i++)
 	{
-		if (s1[i] != s2[i])
 		{
-			return (s1[i] - s2[i]);
+			b =  (s1[i] - s2[i]);
 		}
-		i++;
 	}
 	return (0);
 }
