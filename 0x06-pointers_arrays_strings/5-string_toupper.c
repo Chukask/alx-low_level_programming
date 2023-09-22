ee909fad76515ec7774dadd76b5885ebd6cbc5c2
Chukask <francisoodezuluigbo@igbo@gmail.com>
2023-09-22 08:36:59 -0700

    oh

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,10 +9,10 @@ char *string_toupper(char *n)
 	int i;
 
 	i = 0;
-	while ((n[i]) != '\0')
+	while (n[i])
 	{
 		if (n[i] >= '0' && n[i] <= 'i')
-			n[i] = n[i] - 32;
+		n[i] -= 32;
 		i++;
 	}
 	return (n);
