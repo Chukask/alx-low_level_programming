b61e67e2cc459a113a182b56aa266b5b7c90fa0a
Chukask <francisoodezuluigbo@igbo@gmail.com>
2023-09-22 08:12:07 -0700

    keepo

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,14 +7,10 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-	int b = 0;
-
-	for (i = 0; s1[i] !=  '\0' && b == 0; i++)
+	while (*s1 && *s2 && *s1 == *s2)
 	{
-		{
-			b =  (s1[i] - s2[i]);
-		}
+	s1++;
+	s2++;
 	}
-	return (0);
+	return (*s1 - *s2);
 }
