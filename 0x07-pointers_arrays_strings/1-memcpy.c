3dddd8d0d86cc51066562de62f852a6a2ca2957c
Chukask <francisoodezuluigbo@igbo@gmail.com>
2023-09-26 18:18:01 -0700

    keep

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,7 +1,8 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- *  _memcy - Entry point
+ *  _memcpy - a function that copies memory area
  *  @dest: input
  *  @src: input
  *  @n: input
@@ -12,7 +13,9 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
+	{
 		dest[i] = src[i];
-
+		n--;
+	}
 	return (dest);
 }
